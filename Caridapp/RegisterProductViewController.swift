2a35867b3bb0274ce1b49c7b1c0c6bc6488bc253
Guillermo Williams <guillermowilliams01@gmail.com>
2021-09-14 20:06:30 -0400

    Sign In Interface

---
+++ Caridapp/RegisterProductViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterProductViewController.swift
+//  Caridapp
+//
+//  Created by user190825 on 9/9/21.
+//
+
+
+import UIKit
+
+class RegisterProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
