61962a74f77f01783fa2b000a0db67684a7244cd
jesusej <jesus-e98@hotmail.com>
2021-10-09 23:45:23 -0500

    :construction: Reworking UI of BDA-1

--- Caridapp/Model/ValueObject/Product.swift
+++ Caridapp/Model/ValueObject/Product.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public class Product: Codable {
+    public let upc: Int64;
+    public let itemName: String;
+    public let description: String;
+    public let unitaryWeight: Double;
+    
+    public init (upc: Int64, itemName: String, description: String, unitaryWeight: Double) {
+        self.upc = upc;
+        self.itemName = itemName;
+        self.description = description;
+        self.unitaryWeight = unitaryWeight;
+    }
+}
---
+++ Caridapp/RegisterNewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterNewController.swift
+//  Caridapp
+//
+//  Created by user194082 on 10/9/21.
+//
+
+import UIKit
+
+class RegisterNewViewController: UIViewController {
+    
+    var donation : Donation?
+    
+    
+}
