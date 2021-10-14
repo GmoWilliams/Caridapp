ba333bf759259acd1a797b64f42b63445946c927
Asdertuy <valter.alejandro@hotmail.com>
2021-10-13 22:03:03 -0500

    :lipstick:

--- Caridapp/ViewControllerBDA14-15/VewControllerBDA14_2.swift
+++ Caridapp/ViewControllerBDA14-15/VewControllerBDA14_2.swift
@@ -7,12 +7,50 @@
 
 import UIKit
 
-@IBDesignable class ViewControllerBDA14_2: UIViewController {
+@IBDesignable class ViewControllerBDA14_2: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet var updateExpire: UITextField!
 
+    
+    var line: String?
+    
+    var update: (()->Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateExpire.delegate = self
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(updateLine))
         
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        updateLine()
+        
+        return true
+    }
+    
+    @objc func updateLine() {
+        
+        guard let text = updateExpire.text, !text.isEmpty else {
+            return
+        }
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else{
+            return
+        }
+
+        
+        UserDefaults().set(text, forKey: "line_\(count)")
+        
+        update?()
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
 }
 
