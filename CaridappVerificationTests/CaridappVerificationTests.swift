bfe22687ea81063b13d2a8b6c235ddab4d82c666
Asdertuy <valter.alejandro@hotmail.com>
2021-10-24 14:50:33 -0500

    :construction: working on tests for BDA-14 and BDA-15

---
+++ CaridappVerificationTests/CaridappVerificationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CaridappVerificationTests.swift
+//  CaridappVerificationTests
+//
+//  Created by user196197 on 10/23/21.
+//
+
+import XCTest
+
+@testable import Caridapp
+
+class CaridappVerificationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
